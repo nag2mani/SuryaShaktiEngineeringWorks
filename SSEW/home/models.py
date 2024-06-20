accb7079b8eb1a392cd8b22a7f68db7571bbf389
nag2mani <nag2mani@gmail.com>
2024-06-20 08:25:51 +0530

    Fund In model added and made migration

--- SSEW/home/models.py
+++ SSEW/home/models.py
@@ -5,6 +5,7 @@ from django.contrib.auth.models import User
 # These are my models, created for database.
 
 class Expenses(models.Model):
+    
     CATEGORY_CHOICES = [
         ('salary', 'SALARY'),
         ('adv_salary', 'ADVANCE SALARY'),
@@ -19,7 +20,7 @@ class Expenses(models.Model):
     date_time = models.DateTimeField(auto_now_add=True)
     online_expenses = models.DecimalField(max_digits=10, decimal_places=2)
     cash_expenses = models.DecimalField(max_digits=10, decimal_places=2)
-    name_of_person = models.CharField(max_length=100)
+    name_of_person = models.CharField(max_length=50)
     category = models.CharField(max_length=50, choices=CATEGORY_CHOICES)
     remark = models.TextField(blank=True, null=True)
 
@@ -28,7 +29,20 @@ class Expenses(models.Model):
     #     return f"{self.name_of_person} - {self.online_expenses} - {self.cash_expenses} on {self.date_time.strftime('%Y-%m-%d %H:%M:%S')}"
 
 class FundIn(models.Model):
+    CATEGORY_CHOICES = [
+        ('return_money', 'RETURN MONEY'),
+        ('rubble', 'RUBBLE'),
+        ('stamping_fee', 'STAMPING FEE'),
+        ('battery', 'BATTERY SERVICE'),
+        ('kanta', 'KANTA'),
+        ('repairing', 'Repairing'),
+        ('other', 'OTHERS')
+    ]
+
     date_time = models.DateTimeField(auto_now_add=True)
-    amount = models.DecimalField(max_digits=10, decimal_places=2)
-    source = models.CharField(max_length=100)
+    online_fund = models.DecimalField(max_digits=10, decimal_places=2)
+    cash_fund = models.DecimalField(max_digits=10, decimal_places=2)
+    fund_head = models.CharField(max_length=50)
+    category = models.CharField(max_length=50, choices=CATEGORY_CHOICES)
     remark = models.TextField(blank=True, null=True)
+
