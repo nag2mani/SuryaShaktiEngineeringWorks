0b0a31047ff29981e949a71dfa21ac0315b1b2e4
nag2mani <nag2mani@gmail.com>
2024-06-21 10:09:44 +0530

    added all models but not done migration in the database

--- SSEW/home/models.py
+++ SSEW/home/models.py
@@ -72,5 +72,8 @@ class Inventory(models.Model):
     total_price = models.DecimalField(max_digits=10, decimal_places=2)
     remark = models.TextField(blank=True, null=True)
 
-    def __str__(self):
-        return self.inventory_name
+    # def __str__(self):
+    #     return self.inventory_name
+
+
+
